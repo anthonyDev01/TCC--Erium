4d2cac2e915a15e2d894834013f88c3734407b11
ANTHONY <anthonychukwudiDev@outlook.com>
2023-07-11 18:18:07 -0300

    adiconando funcionalidades da mala

--- src/components/menuBagagem/menuBagagem.tsx
+++ src/components/menuBagagem/menuBagagem.tsx
@@ -1,32 +1,145 @@
 import "./style.css";
+import { useState, useEffect } from "react";
+import Axios from "axios";
+
+const pathImage = "http://localhost:5000/imagens/"; //caminho das imagens
 
 export function MenuBagagem() {
+  const [data, setData] = useState<Array<any>>([]); //estado para armazenar o array inteiro de categorias
+  const [selectedProduct, setSelectedProduct] = useState<any>({
+    //estado onde vai ser armazenado o item clicado
+    nome: "",
+    imagem: "",
+    peso: "",
+    tipos: [],
+  });
+
+  const [amount, setAmount] = useState<number>(0); //estado para armazenar a quantidade de itens selecionados
+  const [index, setIndex] = useState<number>(0); //estado para armazenar o index do array de tipos dos itens
+
+  useEffect(() => {
+    //bucando o array de categorias do servidor
+    const fetchData = async () => {
+      try {
+        const token = localStorage.getItem("token");
+
+        const response = await Axios.get(
+          "http://localhost:5000/pagina-protegida",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`, // validando permissao para acessar a rota
+            },
+          }
+        );
+        setData(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  //funcao para pegar os valores do item que foi clicado
+  const handleCardClick = (item: any) => {
+    setSelectedProduct(item);
+    setAmount(0);
+  };
+
+  //funcao responsavel por calcular o peso do dos itens
+  const calculateWeight = (qtd: number) => {
+    let weight = selectedProduct.peso * qtd;
+    const type = selectedProduct.tipos;
+
+    if (type) {
+      weight = type[index].peso * qtd;
+    }
+
+    if (weight >= 1000) {
+      // verificando se o valor chegou em kilos ou esta em gramas
+      const weightKg = (weight / 1000).toFixed(1);
+      return `${weightKg} kg`;
+    } else {
+      const weightG = weight.toFixed(1);
+      return `${weightG} g`;
+    }
+  };
+
+  //funcao para controlar os tipos do produto (ex: calca tem os tipos: calca leg, c. moletom...)
+  const handleType = (condition: string) => {
+    const type = selectedProduct.tipos;
+    const length = type.length - 1;
+
+    if (condition === "add") {
+      if (index < length) {
+        setIndex(index + 1);
+        setAmount(0);
+      }
+    }
+    if (condition === "remove") {
+      if (index > 0) {
+        setIndex(index - 1);
+        setAmount(0);
+      }
+    }
+  };
+
   return (
-    <div className="menuBagagem">
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
+    <div className="menuContainer">
+      {selectedProduct.nome != "" && (
+        <div className="infoProduto">
+          <div className="imageContainer">
+            <img src={`${pathImage}${selectedProduct.imagem}`} alt="" />
+          </div>
+          <div className="inputContainer">
+            <h2>{selectedProduct.nome}</h2>
+
+            <div className="SubInputContainer">
+              <input
+                type="number"
+                value={amount}
+                onChange={(event) => {
+                  const value = parseInt(event.target.value);
+                  const newValue = value >= 0 ? value : 0;
+                  setAmount(newValue);
+                }}
+              />
+              {selectedProduct.tipos && ( // se o item selecionado tiver tipos mostro os tipos na tela
+                <div className="typeContainer">
+                  <button onClick={() => handleType("remove")}></button>
+                  <div className="types">
+                    {selectedProduct.tipos[index].nome}
+                  </div>
+                  <button onClick={() => handleType("add")}></button>
+                </div>
+              )}
+            </div>
+            <button className="btnAdicionar">Adicionar</button>
+          </div>
+          <span>Peso: {calculateWeight(amount)}</span>
+        </div>
+      )}
+
+      <div className="menuBagagem">
+        {data.length > 0 ? ( //verificando se o array é vazio ou nao
+          data[0].roupas.map(
+            (
+              item: any // renderizando os itens do menu
+            ) => (
+              <div
+                key={item.nome}
+                className="card"
+                onClick={() => handleCardClick(item)}
+              >
+                <img src={`${pathImage}${item.imagem}`} alt="" />
+                <span>{item.nome}</span>
+              </div>
+            )
+          )
+        ) : (
+          <p>Carregando...</p>
+        )}
+      </div>
     </div>
   );
 }
--- src/data/categorias.ts
+++ src/data/categorias.ts
@@ -1,75 +1,273 @@
 const categorias = [
-    {
-      roupas: {
-        calca: {
-          nome: "Calça",
-          imagem: ""
-          
-        },
-        meias: {
-          nome: "Meias",
-          imagem: ""
-        },
-        blusa: {
-          imagem: ""
-        },
-        bermuda: {
-          nome: "Bermuda",
-          imagem: ""
-        },
-        tenis: {
-          nome: "Tênis",
-          imagem: ""
-        },
-        roupas_intimas: {
-          nome: "Roupas íntimas",
-          imagem: ""
-        },
-        bone: {
-          nome: "Boné",
-          imagem: ""
-        },
-        chinelos: {
-          nome: "Chinelos",
-          imagem: ""
-        },
-        touca: {
-          nome: "Touca",
-          imagem: ""
-        },
-        vestido: {
-          nome: "Vestido",
-          imagem: ""
-        },
-        saia: {
-          nome: "Saia",
-          imagem: ""
-        },
-        meia_calca: {
-          nome: "Meia Calça",
-          imagem: ""
-        },
-        camiseta: {
-          nome: "Camiseta",
-          imagem: ""
-        },
-        sutia: {
-          nome: "Sutiã",
-          imagem: ""
-        },
-        body: {
-          nome: "Body",
-          imagem: ""
-        },
-        botas: {
-          nome: "Botas",
-          imagem: ""
-        },
-        cachecol: {
-          nome: "Cachecol",
-          imagem: ""
-        },
-        
+  {
+    roupas: {
+      calca: {
+        nome: "Calça",
+        imagem: "",
+        tipos: [
+          {
+            nome: "Calça Jeans",
+            imagem: "",
+            peso: "500g"
+          },
+          {
+            nome: "Calça Legging",
+            imagem: "",
+            peso: "300g"
+          },
+          {
+            nome: "Calça Social",
+            imagem: "",
+            peso: "600g"
+          },
+          {
+            nome: "Calça Cargo",
+            imagem: "",
+            peso: "700g"
+          },
+          {
+            nome: "Calça de Moletom",
+            imagem: "",
+            peso: "400g"
+          }
+        ]
+      },
+      meias: {
+        nome: "Meias",
+        imagem: "",
+        tipos: [
+          {
+            nome: "Meias Curtas",
+            imagem: "",
+            peso: "50g"
+          },
+          {
+            nome: "Meias Longas",
+            imagem: "",
+            peso: "80g"
+          },
+          {
+            nome: "Meias Esportivas",
+            imagem: "",
+            peso: "70g"
+          },
+          {
+            nome: "Meias de Algodão",
+            imagem: "",
+            peso: "60g"
+          },
+          {
+            nome: "Meias de Lã",
+            imagem: "",
+            peso: "100g"
+          }
+        ]
+      },
+      blusa: {
+        nome: "Blusa",
+        imagem: "",
+        tipos: [
+          {
+            nome: "Blusa de Manga Comprida",
+            imagem: "",
+            peso: "250g"
+          },
+          {
+            nome: "Blusa de Alça",
+            imagem: "",
+            peso: "150g"
+          },
+          {
+            nome: "Blusa de Tricô",
+            imagem: "",
+            peso: "350g"
+          },
+          {
+            nome: "Blusa de Moletom",
+            imagem: "",
+            peso: "400g"
+          },
+          {
+            nome: "Blusa Peplum",
+            imagem: "",
+            peso: "200g"
+          }
+        ]
+      },
+      bermuda: {
+        nome: "Bermuda",
+        imagem: "",
+        tipos: [
+          {
+            nome: "Bermuda Jeans",
+            imagem: "",
+            peso: "400g"
+          },
+          {
+            nome: "Bermuda Cargo",
+            imagem: "",
+            peso: "300g"
+          },
+          {
+            nome: "Bermuda de Praia",
+            imagem: "",
+            peso: "200g"
+          },
+          {
+            nome: "Bermuda de Tecido",
+            imagem: "",
+            peso: "250g"
+          },
+          {
+            nome: "Bermuda Ciclista",
+            imagem: "",
+            peso: "350g"
+          }
+        ]
+      },
+      tenis: {
+        nome: "Tênis",
+        imagem: "",
+        tipos: [
+          {
+            nome: "Tênis Esportivo",
+            imagem: "",
+            peso: "700g"
+          },
+          {
+            nome: "Tênis Casual",
+            imagem: "",
+            peso: "600g"
+          },
+          {
+            nome: "Tênis de Corrida",
+            imagem: "",
+            peso: "800g"
+          },
+          {
+            nome: "Tênis de Skate",
+            imagem: "",
+            peso: "750g"
+          },
+          {
+            nome: "Tênis de Couro",
+            imagem: "",
+            peso: "900g"
+          }
+        ]
+      },
+      roupas_intimas: {
+        nome: "Roupas íntimas",
+        imagem: "",
+        tipos: [
+          {
+            nome: "Sutiã",
+            imagem: "",
+            peso: "100g"
+          },
+          {
+            nome: "Cueca Boxer",
+            imagem: "",
+            peso: "80g"
+          },
+          {
+            nome: "Calcinha",
+            imagem: "",
+            peso: "50g"
+          },
+          {
+            nome: "Cueca Slip",
+            imagem: "",
+            peso: "70g"
+          },
+          {
+            nome: "Camisola",
+            imagem: "",
+            peso: "150g"
+          }
+        ]
+      },
+      bone: {
+        nome: "Boné",
+        imagem: "",
+        peso: "100g"
+      },
+      chinelos: {
+        nome: "Chinelos",
+        imagem: "",
+        tipos: [
+          {
+            nome: "Chinelo de Dedo",
+            imagem: "",
+            peso: "200g"
+          },
+          {
+            nome: "Chinelo Slide",
+            imagem: "",
+            peso: "250g"
+          },
+          {
+            nome: "Chinelo de Pano",
+            imagem: "",
+            peso: "150g"
+          },
+          {
+            nome: "Chinelo de Borracha",
+            imagem: "",
+            peso: "180g"
+          },
+          {
+            nome: "Chinelo de Couro",
+            imagem: "",
+            peso: "300g"
+          }
+        ]
+      },
+      touca: {
+        nome: "Touca",
+        imagem: "",
+        peso: "50g"
+      },
+      vestido: {
+        nome: "Vestido",
+        imagem: "",
+        peso: "400g"
+      },
+      saia: {
+        nome: "Saia",
+        imagem: "",
+        peso: "300g"
+      },
+      meia_calca: {
+        nome: "Meia Calça",
+        imagem: "",
+        peso: "150g"
+      },
+      camiseta: {
+        nome: "Camiseta",
+        imagem: "",
+        peso: "200g"
+      },
+      sutia: {
+        nome: "Sutiã",
+        imagem: "",
+        peso: "100g"
+      },
+      body: {
+        nome: "Body",
+        imagem: "",
+        peso: "250g"
+      },
+      botas: {
+        nome: "Botas",
+        imagem: "",
+        peso: "900g"
+      },
+      cachecol: {
+        nome: "Cachecol",
+        imagem: "",
+        peso: "150g"
       }
     }
-  ];
+  }
+];
